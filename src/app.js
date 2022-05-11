21ca81fa0a69aa8594f335702e582564011e4f61
mtskinner915 <margarettaylor915@gmail.com>
2022-05-11 13:32:51 -0700

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -41,8 +41,19 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-let city = "New York";
-let apiKey = "0f870e1ed1186b4aa36772e8d255cf47";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "0f870e1ed1186b4aa36772e8d255cf47";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-city-form");
+form.addEventListener("submit", handleSubmit);
